e529183f10415c2a71dbd9debb245e1be536690a
Truong Quy <nguyentruongquy75@gmail.com>
2022-05-22 15:04:58 +0700

    Change api key sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -7,8 +7,8 @@ module.exports = ({ env }) => ({
         apiKey: env("SENDGRID_API_KEY"),
       },
       settings: {
-        defaultFrom: "quy.nguyen@siliconstack.com.au",
-        defaultReplyTo: "quy.nguyen@siliconstack.com.au",
+        defaultFrom: "nguyentruongquy75@gmail.com",
+        defaultReplyTo: "nguyentruongquy75@gmail.com",
       },
     },
   },
