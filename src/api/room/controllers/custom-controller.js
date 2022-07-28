bd8e5cc8f8ca69bb7f819e979beb16c25507aa1a
Truong Quy <nguyentruongquy75@gmail.com>
2022-07-28 20:08:29 +0700

    Update search room route

--- src/api/room/controllers/custom-controller.js
+++ src/api/room/controllers/custom-controller.js
@@ -1,5 +1,58 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
+function checkCorrespondRoom(room, checkInDate, checkOutDate) {
+  const bookings = room.bookings;
+  if (checkOutDate) {
+    const existBooking = bookings.some((booking) => {
+      if (!booking.paymentReference) {
+        return false;
+      }
+      const searchCheckInDate = moment(checkInDate);
+      const searchCheckOutDate = moment(checkOutDate);
+      return (
+        searchCheckOutDate.isBetween(
+          booking.checkInDate,
+          booking.checkOutDate,
+          undefined,
+          "[]"
+        ) ||
+        searchCheckInDate.isBetween(
+          booking.checkInDate,
+          booking.checkOutDate,
+          undefined,
+          "[]"
+        ) ||
+        moment(booking.checkInDate).isBetween(
+          searchCheckInDate,
+          searchCheckOutDate,
+          undefined,
+          "[]"
+        ) ||
+        moment(booking.checkOutDate).isBetween(
+          searchCheckInDate,
+          searchCheckOutDate,
+          undefined,
+          "[]"
+        )
+      );
+    });
+
+    return !existBooking;
+  } else {
+    const existBooking = bookings.find((booking) => {
+      const searchCheckIn = moment(checkInDate);
+
+      return searchCheckIn.isBetween(
+        booking.checkInDate,
+        booking.checkOutDate,
+        undefined,
+        "[]"
+      );
+    });
+    return !Boolean(existBooking);
+  }
+}
+
 module.exports = createCoreController("api::room.room", ({ strapi }) => ({
   async analyst(ctx) {
     ctx.query = { ...ctx.query, local: "en" };
@@ -15,6 +68,11 @@ module.exports = createCoreController("api::room.room", ({ strapi }) => ({
   async search(ctx) {
     const query = ctx.query;
 
+    let results = [];
+
+    const searchCheckIn = query.checkInDate;
+    const searchCheckOut = query.checkOutDate;
+
     const rooms = await strapi.db.query("api::room.room").findMany({
       where: {
         ...query.filters,
@@ -22,7 +80,17 @@ module.exports = createCoreController("api::room.room", ({ strapi }) => ({
       populate: query.populate,
     });
 
-    const sanitizedEntity = await this.sanitizeOutput(rooms, ctx);
+    if (searchCheckIn && searchCheckOut) {
+      results = rooms.filter((room) =>
+        checkCorrespondRoom(room, searchCheckIn, searchCheckOut)
+      );
+    } else if (searchCheckIn) {
+      results = rooms.filter((room) =>
+        checkCorrespondRoom(room, searchCheckIn, searchCheckOut)
+      );
+    } else results = rooms;
+
+    const sanitizedEntity = await this.sanitizeOutput(results, ctx);
 
     return this.transformResponse(sanitizedEntity);
   },
